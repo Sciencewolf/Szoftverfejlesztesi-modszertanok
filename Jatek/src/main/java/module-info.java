0342e4e0051d82d2a1847fb89490db30f06f7954
Sciencewolf <aron17marton@gmail.com>
2023-11-06 20:00:08 +0100

    Test

--- Jatek/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.jatek {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires com.almasb.fxgl.all;
-
-    opens com.example.jatek to javafx.fxml;
-    exports com.example.jatek;
-}
