26d7d617853e26509ceef2915ef01e31baef5569
Ádám Karátsony <akaratsony@gmail.com>
2023-11-14 08:31:37 +0100

    Creating the unit test file for the BoardGameModel

---
+++ src/test/java/tablegame/model/BoardGameModelTest.java
@@ -0,0 +1,12 @@
+package tablegame.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class BoardGameModelTest {
+
+
+
+
+}
