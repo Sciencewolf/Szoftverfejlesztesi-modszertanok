a04d6c3aa2b4c1b8d6208a11f8aae6cb902df962
Ádám Karátsony <akaratsony@gmail.com>
2023-11-14 09:43:47 +0100

    Deleting the before version and implemented the selection test

--- src/test/java/tablegame/BoardGameMoveSelectorTest.java
+++ src/test/java/tablegame/BoardGameMoveSelectorTest.java
@@ -9,7 +9,7 @@ import tablegame.model.BoardGameModel;
 import tablegame.model.Position;
 
 import static org.junit.jupiter.api.Assertions.*;
-class BoardGameMoveSelectorTest {
+public class BoardGameMoveSelectorTest {
     BoardGameModel model;
     ReadOnlyObjectWrapper<BoardGameMoveSelector.Game_Phase> phase;
     BoardGameMoveSelector selector;
@@ -17,31 +17,22 @@ class BoardGameMoveSelectorTest {
     @BeforeEach
     public void init() {
         model = new BoardGameModel();
-        phase = new ReadOnlyObjectWrapper<>(BoardGameMoveSelector.Game_Phase.SELECT);
         selector = new BoardGameMoveSelector(model);
     }
 
-    //át kell nézni a move selectort mert:
-    //org.opentest4j.AssertionFailedError:
-    //Expected :INVALID_SELECT
-    //Actual   :CONFIRM_SELECT
     @Test
-    void testSelect() {
-        // Teszteljük a helyes választást
-        Position pos = new Position(1, 1);
-        selector.select(pos);
-
-        // Most hívjuk meg a confirmSelect-et is, hogy átlépjen a READY_TO_MOVE állapotba
-        selector.confirmSelect(pos);
-
-        assertEquals(BoardGameMoveSelector.Game_Phase.READY_TO_MOVE, selector.getPhase());
-        assertFalse(selector.isInvalidSelection());
-
-        // Teszteljük az érvénytelen választást
-        selector.reset(); // Alaphelyzetbe állítjuk az állapotot
-        selector.select(pos);
-        assertEquals(BoardGameMoveSelector.Game_Phase.INVALID_SELECT, selector.getPhase());
-        assertTrue(selector.isInvalidSelection());
+    public void isReadyToMoveWhenNotReady() {
+        System.out.println(selector.getPhase());
+        assertEquals(false, selector.isReadyToMove());
     }
 
+    @Test
+    public void isReadyToMoveWhenSelectFrom() {
+        System.out.println(selector.getPhase());
+        selector.select(new Position(1, 1));
+        assertEquals(false, selector.isReadyToMove());
+    }
+
+
 }
+
