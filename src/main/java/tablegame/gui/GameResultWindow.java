ea0621ea96426a7045933101b62d7da592ad10e2
Sciencewolf <aron17marton@gmail.com>
2023-11-22 23:22:45 +0100

    Result window

---
+++ src/main/java/tablegame/gui/GameResultWindow.java
@@ -0,0 +1,42 @@
+package tablegame.gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class GameResultWindow {
+    public static final String STYLEPATH = "file:src/main/java/tablegame/style/styleResultPage.css";
+    public static final String ICONPATH = "file:src/main/java/tablegame/icon/iconResultPage.png";
+    @FXML
+    public static void createResultWindow() {
+        Text label = new Text("Hello");
+        label.getStyleClass().add("label");
+
+        Button buttonClose = new Button("Close");
+        buttonClose.getStyleClass().add("buttonClose");
+        buttonClose.setOnAction(e -> {
+            Stage stage = (Stage) buttonClose.getScene().getWindow();
+            stage.close();
+        });
+
+        Group group = new Group();
+        group.getChildren().add(label);
+        group.getChildren().add(buttonClose);
+
+        Stage stage = new Stage();
+        Scene scene = new Scene(group);
+        scene.getStylesheets().add(STYLEPATH);
+
+        stage.setScene(scene);
+        stage.setWidth(400);
+        stage.setHeight(200);
+        stage.setTitle("Result");
+        stage.getIcons().add(new Image(ICONPATH));
+        stage.hide();
+        stage.show();
+    }
+}
