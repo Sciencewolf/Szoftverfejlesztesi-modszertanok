6604a4010d3baf1fbb9141501d02317a17ef3bc5
Sciencewolf <aron17marton@gmail.com>
2023-11-27 23:10:28 +0100

    Class to store path variables, added getters

---
+++ src/main/java/tablegame/gui/PathClass.java
@@ -0,0 +1,68 @@
+package tablegame.gui;
+
+public class PathClass {
+    private final String IMAGEPATH1 = "src/main/java/tablegame/images/image.png";
+
+    private final String STYLEGAMESCENEPATH = "file:src/main/java/tablegame/style/styleGameScene.css";
+
+    private final String ICONGAMESCENEPATH = "file:src/main/java/tablegame/icon/iconGameScene.png";
+
+    private final String STYLEINFOPATH = "file:src/main/java/tablegame/style/styleInfo.css";
+
+    private final String ICONINFOPAGEPATH = "file:src/main/java/tablegame/icon/iconInfoPage.png";
+
+    private final String FILEPATH = "src/main/java/tablegame/stats.txt";
+    private final String STYLESTATSPATH = "file:src/main/java/tablegame/style/styleStats.css";
+
+    private final String ICONSTATSPAGEPATH = "file:src/main/java/tablegame/icon/iconStatsPage.png";
+
+    private final String STYLERESULTPAGEPATH = "file:src/main/java/tablegame/style/styleResultPage.css";
+
+    private final String ICONRESULTPAGEPATH = "file:src/main/java/tablegame/icon/iconResultPage.png";
+
+    private final String MENUFXMLPATH= "/fxml/menu.fxml";
+
+    public String getIMAGEPATH1() {
+        return IMAGEPATH1;
+    }
+
+    public String getSTYLEGAMESCENEPATH() {
+        return STYLEGAMESCENEPATH;
+    }
+
+    public String getICONGAMESCENEPATH() {
+        return ICONGAMESCENEPATH;
+    }
+
+    public String getSTYLEINFOPATH() {
+        return STYLEINFOPATH;
+    }
+
+    public String getICONINFOPAGEPATH() {
+        return ICONINFOPAGEPATH;
+    }
+
+    public String getFILEPATH() {
+        return FILEPATH;
+    }
+
+    public String getSTYLESTATSPATH() {
+        return STYLESTATSPATH;
+    }
+
+    public String getICONSTATSPAGEPATH() {
+        return ICONSTATSPAGEPATH;
+    }
+
+    public String getSTYLERESULTPAGEPATH() {
+        return STYLERESULTPAGEPATH;
+    }
+
+    public String getICONRESULTPAGEPATH() {
+        return ICONRESULTPAGEPATH;
+    }
+
+    public String getMENUFXMLPATH() {
+        return MENUFXMLPATH;
+    }
+}
