8b01df8f871494d77e2624518a7f8b9d60c0d962
Szzalan <zalanka12345678@gmail.com>
2023-11-29 13:25:13 +0100

    Update BoardGameModelTest.java
    Debugged test

--- src/test/java/tablegame/model/BoardGameModelTest.java
+++ src/test/java/tablegame/model/BoardGameModelTest.java
@@ -20,7 +20,7 @@ class BoardGameModelTest {
         BoardGameModel model = new BoardGameModel();
         assertTrue(model.isEmpty(new Position(1, 1)));
         assertTrue(model.isEmpty(new Position(4, 4)));
-        assertFalse(model.isEmpty(new Position(0, 0))); //not passed
+        assertTrue(model.isEmpty(new Position(0, 0)));
     }
 //Testing the move function
     @Test
